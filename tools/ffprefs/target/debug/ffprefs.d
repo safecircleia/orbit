b2eee03ba5bc428e38b97d702fd22bbafbb257fe
Tomas <tomas@tomasps.com>
2025-11-13 14:29:01 +0100

    fix: update branding and localization in about dialog, adjust build command, and refine package configuration

--- tools/ffprefs/target/debug/ffprefs.d
+++ tools/ffprefs/target/debug/ffprefs.d
@@ -1,1 +1,1 @@
-/home/tomas/Documents/Github/orbit/tools/ffprefs/target/debug/ffprefs: /home/tomas/Documents/Github/orbit/tools/ffprefs/src/main.rs
+E:\orbit\tools\ffprefs\target\debug\ffprefs.exe: E:\orbit\tools\ffprefs\src\main.rs
